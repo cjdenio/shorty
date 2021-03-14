7739c02c71af4672d174cf79460711cec84ba81d
Caleb Denio <cjdenio44@gmail.com>
2021-03-14 16:57:22 -0400

    🚧 Rusty rust

--- src/db.rs
+++ src/db.rs
@@ -1,5 +1,7 @@
-use redis::Commands;
+use redis::{Commands, RedisError};
+use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize)]
 pub struct ShortyLink {
     pub url: String,
 }
@@ -13,9 +15,26 @@ impl ShortyDb {
         ShortyDb { conn }
     }
 
-    pub fn add_link(&mut self, name: String, link: ShortyLink) -> Result<(), String> {
-        let _: () = self.conn.set(format!("link:{}", name), "howdy").unwrap();
+    pub fn add_link(&mut self, name: &String, link: &ShortyLink) -> Result<(), RedisError> {
+        let link_json = serde_json::to_string(&link).unwrap();
+
+        let _: () = self.conn.set(format!("link:{}", name), link_json)?;
 
         Ok(())
+    }
+
+    pub fn get_link(&mut self, name: &String) -> Result<ShortyLink, String> {
+        let link_json: String = self
+            .conn
+            .get(format!("link:{}", name))
+            .map_err(|e| e.to_string())?;
+
+        // Fall back to URL if not JSON-parsable
+        let link: ShortyLink = match serde_json::from_str(&link_json) {
+            Ok(x) => x,
+            Err(_) => ShortyLink { url: link_json },
+        };
+
+        Ok(link)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,43 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
+// 🏡 Local module imports
 pub mod db;
+use db::{ShortyDb, ShortyLink};
 
+// 👽 External create imports
 #[macro_use]
 extern crate rocket;
 
-use std::env;
+use redis::Client;
+use rocket::{response::Redirect, State};
+use std::{env, sync::RwLock};
 
-use rocket::{response::Redirect, Request};
+struct ShortyState {
+    db: RwLock<ShortyDb>,
+}
 
 #[get("/<name>")]
-fn hello(name: String) -> Redirect {
-    Redirect::temporary(format!("https://github.com/{}", name))
+fn link(state: State<ShortyState>, name: String) -> Option<Redirect> {
+    match state.db.write().unwrap().get_link(&name) {
+        Ok(link) => Some(Redirect::temporary(link.url)),
+        Err(_) => None,
+    }
 }
 
 #[get("/")]
-fn index() -> Option<Redirect> {
+fn index(state: State<ShortyState>) -> Option<Redirect> {
+    state
+        .db
+        .write()
+        .unwrap()
+        .add_link(
+            &String::from("gh"),
+            &ShortyLink {
+                url: String::from("https://github.com/cjdenio"),
+            },
+        )
+        .expect("couldn't create link");
+
     match env::var("ROOT_URL") {
         Ok(url) => Some(Redirect::to(url)),
         Err(_) => None,
@@ -23,13 +45,20 @@ fn index() -> Option<Redirect> {
 }
 
 #[catch(404)]
-fn not_found(req: &Request) -> String {
+fn not_found() -> String {
     String::from("404 not found")
 }
 
 fn main() {
+    let redis_client = Client::open(env::var("DB_URL").expect("Missing DB_URL env variable."))
+        .expect("Error connecting to Redis");
+    let db = ShortyDb::new(redis_client);
+
     rocket::ignite()
-        .mount("/", routes![index, hello])
+        .mount("/", routes![index, link])
         .register(catchers![not_found])
+        .manage(ShortyState {
+            db: RwLock::new(db),
+        })
         .launch();
 }
