c8562be0d365f7df6a21d208cee5c8908c6ec37e
Caleb Denio <cjdenio44@gmail.com>
2021-04-12 01:14:38 -0400

    :fire: Remove favicon and unnecessary import

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from "vite";
 import svelte from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
-export default ({ command, mode }) => {
+export default ({ mode }) => {
   return { plugins: [svelte()], base: mode == "production" ? "/admin" : "/" };
 };
